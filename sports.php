f4d7b67dd31ba43f8e55e3fbe716281fba7bed26
Evellin <evellinsouza2806@gmail.com>
2024-09-06 13:32:58 -0300

    uu

--- sports.php
+++ sports.php
@@ -1,10 +1,10 @@
 <?php
 require_once 'db\config.php';
-require_once 'MVC\Controller\SportsController.php';  // Caminho para o SportsController.php
-require_once 'MVC\Model\SportModel.php';  // Caminho para o SportsController.php
+require_once 'MVC\Controller\SportsController.php';  
+require_once 'MVC\Model\SportModel.php';  
 
 $controller = new SportsController($pdo);
-$sports = $controller->listSports();  // Obtém a lista de esportes
+$sports = $controller->listSports();  
 ?>
 
 <!doctype html>
@@ -12,11 +12,11 @@ $sports = $controller->listSports();  // Obtém a lista de esportes
 
 <head>
     <title>Pesquisa da Geração</title>
-    <!-- Required meta tags -->
+    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
+   
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
