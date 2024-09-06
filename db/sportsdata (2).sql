fe2f149e306b634576ac9fa0c12f217269556a47
ruanaribeiro <ruanaribeiro3@gmail.com>
2024-09-06 07:46:11 -0300

    commit:atualização do banco de dados

--- db/sportsdata (2).sql
+++ db/sportsdata (2).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 04/09/2024 às 19:02
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+-- Tempo de geração: 06/09/2024 às 12:38
+-- Versão do servidor: 10.4.27-MariaDB
+-- Versão do PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -39,6 +39,14 @@ CREATE TABLE `competitors` (
   `team` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Despejando dados para a tabela `competitors`
+--
+
+INSERT INTO `competitors` (`id_competitor`, `name`, `age`, `height`, `weight`, `gender`, `cpf`, `rg`, `team`) VALUES
+(1, 'claudio', 55, 1, 88, 'masc', 132001524, 224441532, 'amarela'),
+(2, 'alessandro', 25, 1.66, 67, 'masc', 1122233344, 11112321, 'laranja');
+
 -- --------------------------------------------------------
 
 --
@@ -56,6 +64,14 @@ CREATE TABLE `locale` (
   `country` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Despejando dados para a tabela `locale`
+--
+
+INSERT INTO `locale` (`id_locale`, `street`, `neighborhood`, `number`, `cep`, `city`, `state`, `country`) VALUES
+(1, 'conceição do monte alegre ', 'villa affini', 520, 19703024, 'paraguacu', 'sao paulo', 'brasil'),
+(2, 'salmen zauy', 'vila nova', 325, 19703025, 'paraguacu paulista', 'sao paulo', 'brasil');
+
 -- --------------------------------------------------------
 
 --
@@ -67,6 +83,14 @@ CREATE TABLE `sport` (
   `modality` varchar(255) NOT NULL,
   `olimpic_year` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `sport`
+--
+
+INSERT INTO `sport` (`id_sport`, `modality`, `olimpic_year`) VALUES
+(1, 'natacao', 2024),
+(2, 'volei', 2024);
 
 -- --------------------------------------------------------
 
@@ -83,6 +107,14 @@ CREATE TABLE `trainer` (
   `rg` int(11) NOT NULL,
   `cpf` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `trainer`
+--
+
+INSERT INTO `trainer` (`id_trainer`, `name`, `age`, `height`, `weight`, `rg`, `cpf`) VALUES
+(1, 'leonardo', 27, 1.8, 77, 110003214, 1221012332),
+(2, 'lucas', 41, 1.7, 69, 102221013, 1030142111);
 
 --
 -- Índices para tabelas despejadas
@@ -120,25 +152,25 @@ ALTER TABLE `trainer`
 -- AUTO_INCREMENT de tabela `competitors`
 --
 ALTER TABLE `competitors`
-  MODIFY `id_competitor` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_competitor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `locale`
 --
 ALTER TABLE `locale`
-  MODIFY `id_locale` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_locale` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `sport`
 --
 ALTER TABLE `sport`
-  MODIFY `id_sport` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_sport` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `trainer`
 --
 ALTER TABLE `trainer`
-  MODIFY `id_trainer` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_trainer` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
